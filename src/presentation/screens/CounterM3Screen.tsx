31cddedc23aaa2577dfe7431ef8535a837bec72b
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-17 08:47:03 -0400

    Se agregó la libreria react-native-vector-icons

--- src/presentation/screens/CounterM3Screen.tsx
+++ src/presentation/screens/CounterM3Screen.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Text, View } from 'react-native';
 import { globalStyles } from '../theme/global.styles';
 import { FAB } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 export const CounterM3Screen = () => {
 
@@ -10,6 +11,8 @@ export const CounterM3Screen = () => {
   return (
     <View style={ globalStyles.centerContainer }>
       <Text style={ globalStyles.title }>{ count }</Text>
+
+      <Icon name="accessibility-outline" size={ 35 } />
 
       <FAB
         label="+1"
