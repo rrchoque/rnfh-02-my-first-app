58d992a6a279b51a1c80b544f7c652022752ce5e
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-04-19 08:03:28 -0400

    Se realizaron ejercicios con el Box Object Model

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { SafeAreaView, Text, View } from 'react-native'
+import { PaperProvider } from 'react-native-paper'
+import IonIcon from 'react-native-vector-icons/Ionicons'
+
 // import { HelloWorldScreen } from './src/presentation/screens/HelloWorldScreen'
 // import { CounterScreen } from './src/presentation/screens/CounterScreen'
-import { PaperProvider } from 'react-native-paper'
-import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen'
-
-import IonIcon from 'react-native-vector-icons/Ionicons'
+// import { CounterM3Screen } from './src/presentation/screens/CounterM3Screen'
+// import { BoxObjectModelScreen } from './src/presentation/screens/BoxObjectModelScreen'
+import { DimensionScreen } from './src/presentation/screens/DimensionScreen'
 
 export const App = () => {
   return (
@@ -16,7 +18,10 @@ export const App = () => {
   >
     <SafeAreaView style={{ flex: 1}}>
       {/* <HelloWorldScreen name="Reynaldo Choque" /> */}
-      <CounterM3Screen />
+      {/* <CounterM3Screen /> */}
+      {/* <CounterScreen /> */}
+      {/* <BoxObjectModelScreen /> */}
+      <DimensionScreen />
     </SafeAreaView>
   </PaperProvider>
 
